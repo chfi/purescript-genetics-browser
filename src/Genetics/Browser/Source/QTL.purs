4e87906b1bc00c978e7436be78cda09032c21f15
Christian Fischer <christian@chfi.se>
2017-03-30 18:20:23 +0200

    Improve QTL feature source

--- src/Genetics/Browser/Source/QTL.purs
+++ src/Genetics/Browser/Source/QTL.purs
@@ -2,40 +2,47 @@ module Genetics.Browser.Source.QTL
        where
 
 import Prelude
-import Text.Parsing.CSV
-import Text.Parsing.Parser
+
+import Text.Parsing.CSV (defaultParsers)
+import Text.Parsing.Parser (parseErrorMessage, runParser)
 import Data.Map as Map
 import Control.Alternative ((<|>))
 import Control.Monad.Aff (Aff)
-import Control.Monad.Eff.Class (liftEff)
-import Control.Monad.Eff.Console (log)
-import Data.Array (fromFoldable)
+import Data.Array (fromFoldable, mapMaybe)
+import Data.Either (Either(..))
 import Data.Foreign (Foreign)
-import Data.HTTP.Method (Method(..))
+import Data.Foreign.Null (writeNull)
+import Data.Function.Uncurried (Fn4, runFn4)
 import Data.Map (Map)
 import Data.Maybe (Maybe)
-import Debug.Trace (traceAny)
 import Genetics.Browser.Feature (Feature(..))
 import Global (readFloat)
-import Network.HTTP.Affjax (affjax, defaultRequest, get)
+import Network.HTTP.Affjax (AJAX, get)
+import Unsafe.Coerce (unsafeCoerce)
 
 type QTLFeature = Feature (score :: Number)
 
+                  -- the first Foreign should be Nullable Feature or similar
+type FetchCallback = ∀ r. Fn4 String Foreign Number Foreign (Aff (ajax :: AJAX | r) Unit)
 
--- this needs to be safer...
--- what a mess.
--- fetch :: ∀ a b. String -> Number -> Number -> Number -> Foreign -> Foreign -> (a -> b) -> Aff _ Unit
+-- Feature should be in this, and 'r' should be the row in Feature r
+type Source r =  String -> Number -> Number
+              -> Number -> Foreign -> Foreign
+              -> FetchCallback -> Aff (ajax :: AJAX | r) Unit
+
+fetch :: Source ()
 fetch chr pmin pmax scale types pool f = do
-  liftEff $ log "getting in purescript"
   res <- get "http://test-gn2.genenetwork.org/api_pre1/qtl/lod2.csv"
   let parsed = runParser res.response defaultParsers.fileHeaded
       results = (map lineToFeature) <$> parsed
-  liftEff $ log $ "GET: " <> res.response
-  traceAny ((fromFoldable <$> results) :: _) (\_ -> pure unit)
-  f res
+  case fromFoldable <$> results of
+    Left  er -> unsafeCoerce $
+                runFn4 f (parseErrorMessage er) writeNull 0.0 writeNull
+    Right fs -> unsafeCoerce $ let fs' = mapMaybe id fs -- filtering away Nothings
+                in runFn4 f "features" (unsafeCoerce fs') 0.0 writeNull
 
 
--- also not very safe or stable (case sensitive, numbers as strings...)
+-- not very safe or stable (case sensitive, numbers as strings...)
 lineToFeature :: Map String String -> Maybe QTLFeature
 lineToFeature m = do
   chr <- Map.lookup "Chr" m
