ad3b8bcbe2451d52f0e9052bf86144e1f086fd73
Christian Fischer <christian@chfi.se>
2017-04-26 16:03:05 +0200

    Temporary fix to serialize Features to something BD-compatible

--- src/Genetics/Browser/GlyphF/Interpret.purs
+++ src/Genetics/Browser/GlyphF/Interpret.purs
@@ -13,12 +13,13 @@ import Data.Foreign (F, Foreign, renderForeignError, toForeign)
 import Data.Maybe (Maybe)
 import Data.Newtype (unwrap)
 import Data.Nullable (toNullable)
-import Genetics.Browser.Feature (Feature)
+import Genetics.Browser.Feature (Feature(..))
 import Genetics.Browser.Glyph (Glyph)
 import Genetics.Browser.GlyphF.Position (glyphToGlyphPosition)
 
+-- TODO: this should be dealt with in Feature, in a safer way
+helper (Feature chr xl xr _) = {chr: chr, min: xl, max: xr}
 
--- writeGlyph' = ?sad
 writeGlyph' :: ∀ a c r. Maybe (Feature c r) -> Glyph a -> Foreign
 writeGlyph' f g = toForeign { "draw": unsafePerformEff <<< \ctx -> Canvas.renderGlyph ctx g
                             , "min": const p.min
@@ -29,27 +30,9 @@ writeGlyph' f g = toForeign { "draw": unsafePerformEff <<< \ctx -> Canvas.render
                             , "toSVG": unsafePerformEff <<< \_ -> SVG.renderGlyph g
                             }
     where p = unwrap $ glyphToGlyphPosition g
-          f' = toNullable f
-          -- unsafeProp :: ∀ x. String -> x -> Prop
-          -- unsafeProp k v = Prop { key: k, value: toForeign v }
-
--- writeGlyph' :: ∀ a c r. Maybe (Feature c r) -> Glyph a -> Foreign
--- writeGlyph' f g = writeObject [ unsafeProp "draw" $ unsafePerformEff <<< \ctx -> Canvas.renderGlyph ctx g
---                               , unsafeProp "min" $ const p.min
---                               , unsafeProp "max" $ const p.max
---                               , unsafeProp "minY" $ const p.minY
---                               , unsafeProp "maxY" $ const p.maxY
---                               , unsafeProp "feature" f'
---                               , unsafeProp "toSVG" $ unsafePerformEff <<< \_ -> SVG.renderGlyph g
---                               ]
---     where p = unwrap $ glyphToGlyphPosition g
---           f' = fromMaybe writeNull (toForeign <$> f)
---           unsafeProp :: ∀ x. String -> x -> Prop
---           unsafeProp k v = Prop { key: k, value: toForeign v }
+          f' = toNullable $ (map helper) f
 
 
--- If the Glyph is wrapped in an F, and there is an error,
--- the error is propagated to the browser and the user is informed
 writeGlyph :: ∀ a c r. Maybe (Feature c r) -> F (Glyph a) -> Foreign
 writeGlyph f fG = case runExcept fG of
   Left errors -> toForeign $ fold $ renderForeignError <$> errors
