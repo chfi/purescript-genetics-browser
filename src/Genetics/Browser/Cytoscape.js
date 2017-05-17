fae5c00450c8e54a90bc318552a3d8ae9e398810
Christian Fischer <christian@chfi.se>
2017-05-17 11:15:22 +0200

    Clean up Cytoscape.js event handlers

--- src/Genetics/Browser/Cytoscape.js
+++ src/Genetics/Browser/Cytoscape.js
@@ -1,3 +1,5 @@
+"use strict";
+
 exports.cytoscape = function(el) {
     return function(eles) {
         return function() {
@@ -25,8 +27,6 @@ exports.cytoscape = function(el) {
                             'opacity': 0.5,
                             'label': 'data(maxLRS)',
                             'line-color': '#ccc'
-                            // 'target-arrow-color': '#ccc',
-                            // 'target-arrow-shape': 'triangle'
                         }
                     }
                 ],
@@ -50,37 +50,11 @@ exports.resize = function(cy) {
     };
 };
 
-exports.setOn = function(cy) {
-    return function(evs) {
+exports.onEventImpl = function(evs) {
+    return function(cy) {
         return function(callback) {
             return function() {
                 cy.on(evs, callback);
-            };
-        };
-    };
-};
-
-exports.setBDOn = function(bd) {
-    return function(cy) {
-        cy.on('tap', function(evt){
-            console.log(evt.target.id);
-            console.log(bd);
-            if (evt.target.id() === 'a') {
-                console.log("going left");
-                bd.setLocation(null, 6400000, 10000000);
-            } else if (evt.target.id() === 'b') {
-                console.log("going right");
-                bd.setLocation(null, 14000000, 20000000);
-            }
-        });
-    };
-};
-
-exports.elesOn = function(evs) {
-    return function(callback) {
-        return function(eles) {
-            return function() {
-                eles.on(evs, callback);
             };
         };
     };
@@ -104,6 +78,12 @@ exports.removeElements = function(eles) {
     };
 };
 
+exports.removeAllElements = function(cy) {
+    return function() {
+        cy.elements().remove();
+    };
+};
+
 exports.filterElements = function(pred) {
     return function(eles) {
         return eles.filter(pred);
@@ -118,38 +98,18 @@ exports.cyFilterElements = function(cy) {
     };
 };
 
-exports.cyFilterByString = function(cy) {
-    return function(chr) {
-        return function() {
-            var es = cy.filter(function(e,y) {
-                return e.group() == "edges" && e.data().lrsLoc.chr != ("Chr" + chr);
-            });
-            es.remove()
-                .targets()
-                .remove();
-        };
-    };
-};
 
-exports.elDataImpl = function(just) {
-    return function(nothing) {
-        return function(el) {
-            var loc = el.data().lrsLoc;
-            console.log(el);
-            console.log("element data:");
-            console.log(el.data());
-            if (loc.chr && loc.pos) {
-                return just({chr: loc.chr,
-                             pos: loc.pos
-                            });
+exports.parseEventImpl = function(left) {
+    return function(right) {
+        return function(ev) {
+            var result = {};
+            result.cy = ev.cy;
+            if (ev.cy === ev.target) {
+                result.target = left(ev.target);
             } else {
-                return nothing;
+                result.target = right(ev.cy);
             }
+            return result;
         };
     };
-};
-
-
-exports.evtTarget = function(evt) {
-    return evt.target;
 };
--- src/Genetics/Browser/Cytoscape.purs
+++ src/Genetics/Browser/Cytoscape.purs
@@ -6,6 +6,7 @@ import Control.Monad.Aff.Console (log)
 import Control.Monad.Eff (Eff, kind Effect)
 import Control.Monad.Eff.Class (liftEff)
 import DOM.HTML.Types (HTMLElement)
+import Data.Either (Either(..))
 import Data.Foreign (Foreign)
 import Data.Function.Uncurried (Fn2, mkFn2)
 import Data.Maybe (Maybe(..))
@@ -17,18 +18,14 @@ import Network.HTTP.Affjax (AJAX, Affjax, get)
 import Unsafe.Coerce (unsafeCoerce)
 
 
-
-
--- from cy docs:
--- elements : An array of elements specified as plain objects. For convenience, this option can alternatively be specified as a promise that resolves to the elements JSON.
-
--- TODO should be an Eff, using DOM at least!!
 foreign import cytoscape :: ∀ eff. HTMLElement
                          -> Nullable (CyCollection CyElement)
                          -> Eff (cy :: CY | eff) Cytoscape
+
 foreign import cyAdd :: ∀ eff. Cytoscape
                      -> CyCollection CyElement
                      -> Eff (cy :: CY | eff) (CyCollection CyElement)
+
 foreign import cyFilter :: ∀ a.
                            Fn2 a Int Boolean
                         -> Cytoscape
@@ -38,17 +35,40 @@ foreign import runLayout :: forall eff.
                             Cytoscape
                          -> Layout
                          -> Eff (cy :: CY | eff) Unit
+
 foreign import resize :: forall eff. Cytoscape -> Eff (cy :: CY | eff) Unit
 
+-- TODO: move to separate module
 newtype Layout = Layout String
 
 circle = Layout "circle"
 
--- ajaxCytoscape :: String -> String -> Eff (ajax :: AJAX) Cytoscape
-ajaxCytoscape :: _
-ajaxCytoscape div url = launchAff $ do
-  resp <- get url :: Affjax _ Foreign
-  pure $ cytoscape div $ unsafeCoerce resp.response
+
+foreign import data CyElement :: Type
+foreign import data CyCollection :: Type -> Type
+foreign import data CyId :: Type
+foreign import data CyEvent :: Type
+
+-- TODO: move all event stuff to separate module
+newtype ParsedEvent = ParsedEvent { cy :: Cytoscape
+                                  , target :: Either CyElement Cytoscape
+                                  }
+
+-- pretty nasty. would be better using Foreign, but we need to compare two things of different types...
+-- maybe something like
+-- _.target >>= parseCy cy <|> parseEl cy
+-- where parseCy :: Cytoscape -> Foreign -> F Cytoscape
+-- and   parseEl :: Cytoscape -> Foreign -> F CyElement
+foreign import parseEventImpl :: forall a b.
+                                 (a -> Either a b)
+                              -> (b -> Either a b)
+                              -> CyEvent
+                              -> ParsedEvent
+
+
+parseEvent :: CyEvent -> ParsedEvent
+parseEvent = parseEventImpl Left Right
+
 
   -- Cytoscape -> String ->
 ajaxAddEles :: _
@@ -58,17 +78,29 @@ ajaxAddEles cy url = launchAff $ do
 
 -- foreign import setOn :: ∀ eff. Cytoscape -> Eff (cy :: CY | eff) Unit
 -- TODO: the callback should really be an Eff too, but w/e
-foreign import setOn :: ∀ cbEff eff.
-                        Cytoscape
-                     -> String
-                     -> (CyEvent -> Eff cbEff Unit)
-                     -> Eff (cy :: CY | eff) Unit
+foreign import onEventImpl :: ∀ eff a.
+                              Cytoscape
+                           -> String
+                           -> (CyEvent -> Eff (cy :: CY | eff) a)
+                           -> Eff (cy :: CY | eff) Unit
+                     --    Cytoscape
+                     --    -> String
+                     -- -> (CyEvent -> Eff cbEff Unit)
+                     -- -> Eff (cy :: CY | eff) Unit
 
-onClick :: ∀ cbEff eff.
+onEvent :: forall a eff.
            Cytoscape
-        -> (CyEvent -> Eff cbEff Unit)
+        -> String
+        -> (ParsedEvent -> Eff (cy :: CY | eff) a)
         -> Eff (cy :: CY | eff) Unit
-onClick cy = setOn cy "click"
+onEvent cy ev f = onEventImpl cy ev (f <<< parseEvent)
+
+
+onClick :: ∀ eff.
+           Cytoscape
+        -> (ParsedEvent -> Eff (cy :: CY | eff) Unit)
+        -> Eff (cy :: CY | eff) Unit
+onClick cy = onEvent cy "click"
 
 
 -- this doesn't work -- has to keep track of removed elements, or reload elements from scratch.
@@ -79,52 +111,11 @@ resetFilter cy = cyAdd cy $ cyFilter (mkFn2 $ \_ _ -> true) cy
 
 
 
--- foreign import setBDOn :: ∀ eff. Biodalliance -> Cytoscape -> Eff (cy :: CY | eff) Unit
-
--- need to be able to specify stylesheet, elements. other config?
-{-
-cy.add( eleObj )
-Add a specified element to the graph.
-eleObj A plain object that specifies the element.
-cy.add( eleObjs )
-Add the specified elements to the graph.
-eleObjs An array of elements specified by plain objects.
-cy.add( eles )
-Add the specified elements to the graph.
-eles A collection of elements.
-
--- these all return the added elements...
--- ... meaning `cy` is modified.
-
--- `Collections` are immutable.
--}
--- TODO: we really want this to connect with BD, so Features should be here too.
--- would simplify the BD<->Cy interface, at least.
-foreign import data CyElement :: Type
-foreign import data CyCollection :: Type -> Type
-foreign import data CyId :: Type
-foreign import data CyEvent :: Type
--- foreign import
-
-
--- cytoscape events (there are a bunch, we only need a few for now) see http://js.cytoscape.org/#events
--- data CyEvent = OnClick
-
--- newtype CyEvent = CyEvent Cytoscape target type namespace timestamp
-
--- cySetOnClick :: Cytoscape ->
-
-
--- there are also collection events, which I think may be more important...
--- http://js.cytoscape.org/#events/collection-events
--- e.g. events for selecting an element from a collection.
-
--- effectfully removing elements from a collection...
--- removing doesn't delete the elements, it only removes them from the visible graph. hence Eff
-
 foreign import removeElements :: ∀ eff.
                                  CyCollection CyElement
                               -> Eff (cy :: CY | eff) (CyCollection CyElement)
+
+foreign import removeAllElements :: forall eff. Cytoscape -> Eff (cy :: CY | eff) Unit
 
 foreign import filterElements :: ∀ a.
                                  Fn2 a Int Boolean
@@ -134,55 +125,7 @@ foreign import filterElements :: ∀ a.
 filterElements' :: ∀ a. (a -> Int -> Boolean) -> CyCollection a -> CyCollection a
 filterElements' p = filterElements (mkFn2 p)
 
-foreign import elesOn :: ∀ eff. String
-                      -> (CyEvent -> Unit)
-                      -> CyCollection CyElement
-                      -> Eff (cy :: CY | eff) Unit
--- foreign import isRemoved :: ∀ eff. CyElement -> Eff (cy :: CY | eff) Boolean
--- isInside :: ∀ eff. CyElement -> Eff (cy :: CY | eff) Boolean
--- isInside el = not <$> isRemoved el
--- foreign import restoreElements :: ∀ a eff. CyCollection a -> Eff (cy :: CY | eff) Unit
--- foreign import cloneElements :: ∀ a eff. CyCollection a ->
-
--- TODO: there's more to move() than this.
-
--- foreign import moveElements :: ∀ a. CyCollection a -> CyId -> CyCollection a
--- set function as callback
--- foreign import elesOn :: ∀ a b eff. CyCollection a -> (CyEvent -> b) -> Eff (cy :: CY | eff) Unit
--- foreign import eleId :: CyElement -> CyId
-
--- data CyThing a  = CyCore Cytoscape
---                 | CyCollection a
---                 | CyLayout
---                 | CyAni
---                 | CyThing a
-
 foreign import cyFilterElements :: ∀ eff.
                                    Cytoscape
                                 -> (CyElement -> Boolean)
                                 -> Eff (cy :: CY | eff) (CyCollection CyElement)
-
-
-
--- Filters by chromosome -- by appending "Chr" to the current chromosome...
--- TODO: terrible!
-foreign import cyFilterByString :: ∀ eff.
-                                   Cytoscape
-                                -> String
-                                -> Eff (cy :: CY | eff) Unit
-
-
--- TODO: this is shitty and unsafe
--- it doesn't even take the coordinate unit into account!
--- CyElement should probably have the extra data in a type parameter
-type P = { chr :: String, pos :: MBp }
-foreign import elDataImpl :: (P -> Maybe P) -> (Maybe P)
-                          -> CyElement
-                          -> Maybe P
-
-
-elData :: CyElement -> Maybe P
-elData = elDataImpl Just Nothing
-
--- this type is false: sometimes the evt target is the core, or a collection. maybe.
-foreign import evtTarget :: CyEvent -> CyElement
