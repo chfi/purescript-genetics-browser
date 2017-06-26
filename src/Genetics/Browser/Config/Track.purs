ff7734998a31cb18fcc665a0954ec8de8796ce8a
Christian Fischer <christian@chfi.se>
2017-06-26 17:09:48 +0200

    Add basic/placeholder config type for Cy.js graphs

--- src/Genetics/Browser/Config/Track.purs
+++ src/Genetics/Browser/Config/Track.purs
@@ -1,21 +1,72 @@
 module Genetics.Browser.Config.Track
+       ( BDTrackConfig
+       , makeBDTrack
+       , validateBDConfig
+       , CyGraphConfig
+       , makeCyTrack
+       , validateCyConfig
+       , TrackType
+       )
        where
 
 import Prelude
-import Data.Argonaut (Json, _Object)
+import Data.Argonaut (Json, _Object, _String)
 import Data.Either (Either(..))
 import Data.Foreign (Foreign, toForeign)
 import Data.Lens ((^?))
 import Data.Lens.Index (ix)
 import Data.Maybe (Maybe(..))
 
--- TODO add some BD validation
+
+newtype TrackType = TrackType String
+
+-- derive instance newtypeTrackType :: Newtype TrackType _
+derive instance eqTrackType :: Eq TrackType
+derive instance ordTrackType :: Ord TrackType
+
+bdTrack = TrackType "BDTrack"
+cyGraph = TrackType "CyGraph"
+
+getTrackType :: Json -> Maybe TrackType
+getTrackType json = case json ^? _Object <<< ix "trackType" <<< _String of
+  -- must be better ways of encoding this. this is ripe for programmer error
+  Just "BDTrack" -> Just bdTrack
+  Just "CyGraph" -> Just cyGraph
+  _ -> Nothing
+
+
 newtype BDTrackConfig = BDTrackConfig Foreign
 
-makeBDTrack :: ∀ r. { name :: String | r } -> BDTrackConfig
-makeBDTrack = (BDTrackConfig <<< toForeign)
+makeBDTrack :: ∀ r. { trackType :: TrackType, name :: String | r } -> BDTrackConfig
+makeBDTrack = BDTrackConfig <<< toForeign
 
 validateBDConfig :: Json -> Either String BDTrackConfig
-validateBDConfig json = case json ^? _Object <<< ix "name" of
-  Nothing -> Left $ "BD track config does not have a name"
-  Just c  -> Right $ BDTrackConfig $ toForeign json
+validateBDConfig json =
+  case getTrackType json of
+    Nothing -> Left $ "Track has no type"
+    Just cyGraph -> Left $ ""
+    Just bdTrack -> case json ^? _Object <<< ix "name" of
+      Nothing -> Left $ "BD track config does not have a name"
+      Just c  -> Right $ BDTrackConfig $ toForeign json
+
+-- Maybe this should be in another file (one not called 'Track')
+newtype CyGraphConfig = CyGraphConfig Foreign
+
+makeCyTrack :: ∀ r. { trackType :: TrackType, elementsUri :: String } -> CyGraphConfig
+makeCyTrack = CyGraphConfig <<< toForeign
+
+-- this and validateBDConfig must be able to be simplified;
+-- there are two levels of validation, first checking it's a track at all,
+-- then validating the specific track type
+validateCyConfig :: Json -> Either String CyGraphConfig
+validateCyConfig json = do
+  case getTrackType json of
+    Nothing -> Left $ "Track has no type"
+    Just bdTrack -> Left $ ""
+    Just cyGraph -> case json ^? _Object <<< ix "elementsUri" of
+      Nothing -> Left $ "cy graph config does not have an elementsUri"
+      Just c  -> Right $ CyGraphConfig $ toForeign json
+
+-- TODO: combine validateBDConfig and validateCyConfig
+-- validateTrackConfig :: Json -> Either String (Either BDTrackConfig CyGraphConfig)
+-- validateTrackConfig :: Json -> Either String (Either3 BDTrackConfig PSTrackConfig CyGraphConfig)
