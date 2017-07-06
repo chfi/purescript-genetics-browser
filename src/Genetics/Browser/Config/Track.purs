6fa6e7f86ad16c07ac15d1331608cd84ed57c551
Christian Fischer <christian@chfi.se>
2017-07-06 16:57:41 +0200

    Rewrite TrackType to be a regular sum type, so the totality checker can help

--- src/Genetics/Browser/Config/Track.purs
+++ src/Genetics/Browser/Config/Track.purs
@@ -6,6 +6,8 @@ module Genetics.Browser.Config.Track
        , makeCyTrack
        , validateCyConfig
        , TrackType
+       , TracksMap
+       , validateConfigs
        )
        where
 
@@ -13,6 +15,8 @@ import Prelude
 import Data.Argonaut (Json, _Array, _Object, _String)
 import Data.Either (Either(..))
 import Data.Foreign (Foreign, toForeign)
+import Data.Generic.Rep (class Generic)
+import Data.Generic.Rep.Show (genericShow)
 import Data.Lens ((^?))
 import Data.Lens.Index (ix)
 import Data.Maybe (Maybe(..), maybe)
@@ -20,64 +24,56 @@ import Data.StrMap (StrMap)
 import Data.Traversable (sequence)
 
 
-newtype TrackType = TrackType String
-
 newtype TracksMap = TracksMap (StrMap Json)
 
 getConfigs :: TracksMap -> TrackType -> Either String (Array Json)
-getConfigs (TracksMap ts) (TrackType tt) =
-  maybe (Left "Incorrect trackType") Right $ ts ^? ix tt <<< _Array
+getConfigs (TracksMap ts) tt =
+  maybe (Left "Incorrect trackType") Right $ ts ^? ix (show tt) <<< _Array
 
--- derive instance newtypeTrackType :: Newtype TrackType _
+data TrackType = BDTrack | CyGraph
+
 derive instance eqTrackType :: Eq TrackType
 derive instance ordTrackType :: Ord TrackType
+derive instance genericTrackType :: Generic TrackType _
+-- derive instance showTrackType :: Show TrackType
+instance showTrackType :: Show TrackType where
+  show = genericShow
 
-bdTrack :: TrackType
-bdTrack = TrackType "BDTrack"
-cyGraph :: TrackType
-cyGraph = TrackType "CyGraph"
+readTrackType :: String -> Maybe TrackType
+readTrackType "BDTrack" = Just BDTrack
+readTrackType "CyGraph" = Just CyGraph
+readTrackType _ = Nothing
+
 
 getTrackType :: Json -> Maybe TrackType
-getTrackType json = case json ^? _Object <<< ix "trackType" <<< _String of
-  -- must be better ways of encoding this. this is ripe for programmer error
-  Just "BDTrack" -> Just bdTrack
-  Just "CyGraph" -> Just cyGraph
-  _ -> Nothing
+getTrackType json = readTrackType =<< json ^? _Object <<< ix "trackType" <<< _String
 
 
 newtype BDTrackConfig = BDTrackConfig Foreign
 
-makeBDTrack :: ∀ r. { trackType :: TrackType, name :: String | r } -> BDTrackConfig
+makeBDTrack :: ∀ r. { name :: String | r } -> BDTrackConfig
 makeBDTrack = BDTrackConfig <<< toForeign
 
 validateBDConfig :: Json -> Either String BDTrackConfig
-validateBDConfig json =
-  case getTrackType json of
-    Nothing -> Left $ "Track has no type"
-    Just (TrackType "CyGraph") -> Left $ "Is CyGraph"
-    Just (TrackType "BDTrack") -> case json ^? _Object <<< ix "name" of
-      Nothing -> Left $ "BD track config does not have a name"
-      Just c  -> Right $ BDTrackConfig $ toForeign json
-    Just _ -> Left $ "Track has unrecognized type"
+validateBDConfig json = case json ^? _Object <<< ix "name" of
+  Nothing -> Left $ "BD track config does not have a name"
+  Just c  -> Right $ BDTrackConfig $ toForeign json
+
 
 -- Maybe this should be in another file (one not called 'Track')
 newtype CyGraphConfig = CyGraphConfig Foreign
 
-makeCyTrack :: ∀ r. { trackType :: TrackType, elementsUri :: String | r } -> CyGraphConfig
+makeCyTrack :: ∀ r. { elementsUri :: String | r } -> CyGraphConfig
 makeCyTrack = CyGraphConfig <<< toForeign
 
 -- this and validateBDConfig must be able to be simplified;
 -- there are two levels of validation, first checking it's a track at all,
 -- then validating the specific track type
 validateCyConfig :: Json -> Either String CyGraphConfig
-validateCyConfig json = do
-  case getTrackType json of
-    Nothing -> Left $ "Track has no type"
-    Just (TrackType "BDTrack") -> Left $ "Is BD track"
-    Just (TrackType "CyGraph") -> case json ^? _Object <<< ix "elementsUri" of
-      Nothing -> Left $ "cy graph config does not have an elementsUri"
-      Just c  -> Right $ CyGraphConfig $ toForeign json
-    Just _ -> Left $ "Track has unrecognized type"
+validateCyConfig json = case json ^? _Object <<< ix "elementsUri" of
+  Nothing -> Left $ "cy graph config does not have an elementsUri"
+  Just c  -> Right $ CyGraphConfig $ toForeign json
+
 
 -- TODO: combine validateBDConfig and validateCyConfig
 -- validateTrackConfig :: Json -> Either String (Either BDTrackConfig CyGraphConfig)
@@ -92,10 +88,8 @@ foldConfigs :: forall a b.
                , cyGraphs :: Array (Either String b)
                }
 foldConfigs bdFun cyFun tm = { bdTracks, cyGraphs }
-  where bdTracks = map (_ >>= bdFun) $ sequence $ getConfigs tm bdTrack
-        cyGraphs = map (_ >>= cyFun) $ sequence $ getConfigs tm cyGraph
-        test :: _
-        test = map (sequence >=> bdFun) getConfigs tm bdTrack
+  where bdTracks = map (_ >>= bdFun) $ sequence $ getConfigs tm BDTrack
+        cyGraphs = map (_ >>= cyFun) $ sequence $ getConfigs tm CyGraph
  -- there's /probably/ a nicer way to d this
 
 
