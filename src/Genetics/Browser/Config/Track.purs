df301ff002b9299094705b23c073a242ce1bc408
Christian Fischer <christian@chfi.se>
2017-07-07 16:03:57 +0200

    Clean up Track config module

--- src/Genetics/Browser/Config/Track.purs
+++ src/Genetics/Browser/Config/Track.purs
@@ -1,11 +1,6 @@
 module Genetics.Browser.Config.Track
        ( BDTrackConfig
-       , makeBDTrack
-       , validateBDConfig
        , CyGraphConfig
-       , makeCyTrack
-       , validateCyConfig
-       , TrackType
        , TracksMap
        , validateConfigs
        )
@@ -13,7 +8,9 @@ module Genetics.Browser.Config.Track
 
 import Prelude
 import Data.Argonaut (Json, _Array, _Object, _String)
+import Data.Array ((:))
 import Data.Either (Either(..))
+import Data.Foldable (foldr)
 import Data.Foreign (Foreign, toForeign)
 import Data.Generic.Rep (class Generic)
 import Data.Generic.Rep.Show (genericShow)
@@ -35,7 +32,6 @@ data TrackType = BDTrack | CyGraph
 derive instance eqTrackType :: Eq TrackType
 derive instance ordTrackType :: Ord TrackType
 derive instance genericTrackType :: Generic TrackType _
--- derive instance showTrackType :: Show TrackType
 instance showTrackType :: Show TrackType where
   show = genericShow
 
@@ -44,15 +40,15 @@ readTrackType "BDTrack" = Just BDTrack
 readTrackType "CyGraph" = Just CyGraph
 readTrackType _ = Nothing
 
-
 getTrackType :: Json -> Maybe TrackType
 getTrackType json = readTrackType =<< json ^? _Object <<< ix "trackType" <<< _String
 
 
-newtype BDTrackConfig = BDTrackConfig Foreign
+-- TODO: combine validateBDConfig and validateCyConfig
+-- validateTrackConfig :: Json -> Either String (Either BDTrackConfig CyGraphConfig)
+-- validateTrackConfig :: Json -> Either String (Either3 BDTrackConfig PSTrackConfig CyGraphConfig)
 
-makeBDTrack :: ∀ r. { name :: String | r } -> BDTrackConfig
-makeBDTrack = BDTrackConfig <<< toForeign
+newtype BDTrackConfig = BDTrackConfig Foreign
 
 validateBDConfig :: Json -> Either String BDTrackConfig
 validateBDConfig json = case json ^? _Object <<< ix "name" of
@@ -60,41 +56,44 @@ validateBDConfig json = case json ^? _Object <<< ix "name" of
   Just c  -> Right $ BDTrackConfig $ toForeign json
 
 
--- Maybe this should be in another file (one not called 'Track')
 newtype CyGraphConfig = CyGraphConfig Foreign
 
-makeCyTrack :: ∀ r. { elementsUri :: String | r } -> CyGraphConfig
-makeCyTrack = CyGraphConfig <<< toForeign
-
--- this and validateBDConfig must be able to be simplified;
--- there are two levels of validation, first checking it's a track at all,
--- then validating the specific track type
 validateCyConfig :: Json -> Either String CyGraphConfig
 validateCyConfig json = case json ^? _Object <<< ix "elementsUri" of
   Nothing -> Left $ "cy graph config does not have an elementsUri"
   Just c  -> Right $ CyGraphConfig $ toForeign json
 
 
--- TODO: combine validateBDConfig and validateCyConfig
--- validateTrackConfig :: Json -> Either String (Either BDTrackConfig CyGraphConfig)
--- validateTrackConfig :: Json -> Either String (Either3 BDTrackConfig PSTrackConfig CyGraphConfig)
+
+type ValidatedConfigs a = { errors :: Array String
+                          , results :: Array a
+                          }
+
+foldErrors :: forall e r
+            . Array (Either e r)
+           -> { errors :: Array e
+              , results :: Array r
+              }
+foldErrors = foldr (\c er@{errors, results} ->
+                                case c of Left  e -> er { errors  = (e : errors)}
+                                          Right r -> er { results = (r : results)}
+                   ) { errors: [], results: [] }
 
 
-foldConfigs :: forall a b.
-               (Json -> Either String a)
-            -> (Json -> Either String b)
-            -> TracksMap
-            -> { bdTracks :: Array (Either String a)
-               , cyGraphs :: Array (Either String b)
-               }
-foldConfigs bdFun cyFun tm = { bdTracks, cyGraphs }
-  where bdTracks = map (_ >>= bdFun) $ sequence $ getConfigs tm BDTrack
-        cyGraphs = map (_ >>= cyFun) $ sequence $ getConfigs tm CyGraph
- -- there's /probably/ a nicer way to d this
+foldConfig :: forall a
+             . TracksMap
+            -> TrackType
+            -> (Json -> Either String a)
+            -> ValidatedConfigs a
+foldConfig tm tt f = foldErrors $ map (_ >>= f) $ sequence $ getConfigs tm tt
 
 
 validateConfigs :: TracksMap
-                -> { bdTracks :: Array (Either String BDTrackConfig)
-                   , cyGraphs :: Array (Either String CyGraphConfig)
+                -> { bdTracks :: ValidatedConfigs BDTrackConfig
+                   , cyGraphs :: ValidatedConfigs CyGraphConfig
                    }
-validateConfigs = foldConfigs validateBDConfig validateCyConfig
+validateConfigs tm = { bdTracks
+                     , cyGraphs
+                     }
+  where bdTracks = foldConfig tm BDTrack validateBDConfig
+        cyGraphs = foldConfig tm CyGraph validateCyConfig
