e08d8891726e162b8cdbb9dca0550867183caa6d
Christian Fischer <christian@chfi.se>
2017-09-07 18:17:48 +0200

    Implement IPFS fetch and source

--- src/Genetics/Browser/Biodalliance/Source/IPFS.purs
+++ src/Genetics/Browser/Biodalliance/Source/IPFS.purs
@@ -2,21 +2,35 @@ module Genetics.Browser.Biodalliance.Source.IPFS where
 
 import Prelude
 
-import Genetics.Browser.Biodalliance.Source (createSource)
+import Control.Monad.Aff (Aff)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Eff.Exception (EXCEPTION, Error, error, throw)
+import Control.Monad.Error.Class (throwError)
+import Data.Maybe (Maybe(..))
+import Genetics.Browser.Biodalliance.Source (FetchFunction, ForeignSourceBase, Source, createSource)
 import IPFS (IPFS, IPFSEff)
 import IPFS.Files as Files
+import IPFS.Types (IPFSEff, IPFSPath(..))
 import Node.Encoding (Encoding(..))
+import Node.Stream as Stream
 
 
-ipfsFetch :: ∀ a.
+ipfsFetch :: ∀ a eff.
              IPFS
           -> String
           -> (String -> Maybe a)
-          -> FetchFunction (Aff _) (Maybe a)
-          -- -> Chr -> Bp -> Bp -> (Aff _ a)
-ipfsFetch ipfs path parser = \chr min max -> do
-  stream <- Files.cat ipfs path
-  parser <$> liftEff $ Stream.readString stream
+          -> FetchFunction (Aff ( ipfs :: IPFSEff
+                                , exception :: EXCEPTION | eff ) ) a
+ipfsFetch ipfs path parse = \chr min max -> do
+  stream <- Files.cat ipfs (IPFSPathString path)
+
+  liftEff $ Stream.pause stream
+
+  file <- liftEff $ Stream.readString stream Nothing UTF8
+
+  case file >>= parse of
+    Just a  -> pure a
+    Nothing -> throwError $ error "Failed"
 
 
 ipfsSource :: ∀ a.
@@ -24,6 +38,6 @@ ipfsSource :: ∀ a.
            -> String
            -> (String -> Maybe a)
            -> ForeignSourceBase
-           -> Source (Maybe a)
+           -> Source a
 ipfsSource ipfs path parser fsb =
   createSource fsb $ ipfsFetch ipfs path parser
