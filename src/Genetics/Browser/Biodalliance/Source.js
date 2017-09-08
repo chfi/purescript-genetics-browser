0cc2839607d74178baca4a3c9027b323f24f9e3b
Christian Fischer <christian@chfi.se>
2017-09-08 16:50:50 +0200

    Actually fix createSourceImpl

--- src/Genetics/Browser/Biodalliance/Source.js
+++ src/Genetics/Browser/Biodalliance/Source.js
@@ -4,11 +4,10 @@ exports.dummySourceBase = function(x) {
     return new Object();
 };
 
-exports.createSourceImpl = function(ForeignSourceBase) {
+exports.createSourceImpl = function(FeatureSourceBase) {
     return function(fetchFun) {
         var Source = {};
-
-        ForeignSourceBase.call(Source);
+        var fsb = FeatureSourceBase.call(Source);
 
         Source.fetch = function(chr, min, max, scale, types, pool, callback) {
             fetchFun(chr)(min)(max)().then(function(result) {
