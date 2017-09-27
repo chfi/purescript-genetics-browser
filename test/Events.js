138c2e6901de90bbb7880351b988d68d50bfe8a0
Christian Fischer <christian@chfi.se>
2017-09-27 19:48:12 +0200

    Start on TrackSource tests

--- test/Events.js
+++ test/Events.js
@@ -1,3 +1,22 @@
+
+
+exports.trackSource1 = { eventName: "event1",
+                         eventTemplate: {"data": "String"},
+                         rawTemplate: {"x": {"y1": "data",
+                                             "y2": "other"}}
+                       };
+
+exports.rawEvent1 = {"x": {"y1": "this is a string",
+                           "y2": true }
+                    };
+
+var sink1Fun = function(json) {
+    return json.data || "it broke";
+};
+
+exports.trackSink1 = { eventName: "event1",
+                       eventFun: sink1Fun };
+
 exports.stringJson = "this is a string";
 exports.numberJson = 123.0;
 exports.recordJson = { "inner": { pos: 1.23, name: "recordJson" },
--- test/Events.purs
+++ test/Events.purs
@@ -12,72 +12,27 @@ import Data.Lens.Index (ix)
 import Data.Maybe (Maybe(..))
 import Data.Symbol (SProxy(..))
 import Data.Variant (Variant, expand, inj, prj)
-import Genetics.Browser.Events.TrackSink (TrackSink, appendTrackSinks, applyTrackSink, emptyTrackSink, mkTrackSink)
-import Genetics.Browser.Events.TrackSource (TrackSource, appendTrackSources, applyTrackSource, emptyTrackSource)
+import Genetics.Browser.Events.TrackSink (SinkConfig, makeTrackSink)
+import Genetics.Browser.Events.TrackSource (SourceConfig, makeTrackSource, runTrackSource)
+import Genetics.Browser.Events.Types (Event)
 import Test.Spec (Spec, describe, it)
 import Test.Spec.Assertions (fail, shouldEqual)
 
 
+foreign import sourceConfig1 :: SourceConfig
+foreign import sinkConfig1 :: SinkConfig String
+foreign import rawEvent1 :: Json
 
+trackSource1 = makeTrackSource sourceConfig1
+trackSink1 = makeTrackSink sinkConfig1
 
--- type Inner = { pos :: Number, name :: String }
--- type Outer = { inner :: Inner, number :: Number }
-
-
-foreign import stringJson :: Json
-foreign import numberJson :: Json
-foreign import recordJson :: Json
-
--- record :: Outer
--- record = { "inner": { pos: 1.23, name: "recordJson" }, "number": 456.0 }
-
-
+event1 :: Maybe (Array Event)
+event1 = do
+  source <- trackSource1
+  pure $ runTrackSource source rawEvent1
 
 spec :: forall e. Spec e Unit
 spec = do
-  describe "Event Input Handlers" do
-    it "can be extended without affecting the existing handlers" do
+  describe "Event sources Handlers" do
+    it "can parse raw events" do
       true `shouldEqual` true
---       let handler2 :: TrackSink (stringTest :: String, test :: Unit)
---                                    (stringTest :: String -> String, test :: Unit -> String) String
---           handler2 = appendTrackSink (SProxy :: SProxy "test") (const "unit") stringInH
---       applyTrackSink stringInH input `shouldEqual` applyTrackSink handler2 input
-
---     it "can handle subsets of its input event type" do
---       let inputBig :: Variant (stringTest :: String, numTest :: Number)
---           inputBig = expand input
---       applyTrackSink stringNumInH input `shouldEqual` applyTrackSink stringNumInH inputBig
-
-
---   describe "Event Output Handlers" do
---     it "can parse things" do
---       let l = fromFoldable $ applyTrackSource jsonToStringH stringJson
---       case l of
---         [h] -> prj (SProxy :: SProxy "stringTest") h `shouldEqual` Just "this is a string"
---         _   -> fail "TrackSource didn't parse string"
-
-
---     it "can be extended" do
-
---       let lStr1 = fromFoldable $ applyTrackSource jsonToStringH stringJson
---           lStr2 = fromFoldable $ applyTrackSource jsonToStringNumH stringJson
---           lNum = fromFoldable $ applyTrackSource jsonToStringNumH numberJson
-
---       case lStr2 of
---         [h'] -> prj (SProxy :: SProxy "stringTest") h' `shouldEqual` Just "this is a string"
---         _    -> fail "Extended TrackSource didn't parse string"
-
---       case lNum of
---         [h] -> prj (SProxy :: SProxy "numTest") h `shouldEqual` Just 123.0
---         _   -> fail "TrackSource didn't parse number"
-
---   describe "Connecting Input and Output Handlers" do
---     it "Input handlers and output handlers can be connected if their rows match" do
-
-
-
-
---       fail "not implemented"
-
---     it "The Input type can be a superset of the Output type" do
---       fail "not implemented"
