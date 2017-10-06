d82958959506aaab5e4ef952e75250bb54e72fe0
Christian Fischer <christian@chfi.se>
2017-10-06 13:55:00 +0200

    Move TrackSink validation to Either String, check to see that no two event names overlap

--- src/Genetics/Browser/Events/TrackSink.purs
+++ src/Genetics/Browser/Events/TrackSink.purs
@@ -9,6 +9,7 @@ module Genetics.Browser.Events.TrackSink where
 
 import Prelude
 
+import Control.Alternative ((<|>))
 import Control.Monad.Aff (Aff, Canceler, forkAff)
 import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Aff.Bus (BusRW)
@@ -16,16 +17,19 @@ import Control.Monad.Aff.Bus as Bus
 import Control.Monad.Eff (Eff, kind Effect)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Unsafe (unsafeCoerceEff)
+import Control.Monad.Except (throwError)
 import Control.Monad.Rec.Class (forever)
 import Control.MonadPlus (guard)
 import Data.Argonaut (Json)
-import Data.Foldable (foldMap, length)
+import Data.Either (Either)
+import Data.Foldable (any, foldMap, foldr, length)
 import Data.List (List)
 import Data.Maybe (Maybe(..))
 import Data.Monoid (class Monoid)
 import Data.Newtype (class Newtype, unwrap)
 import Data.StrMap (StrMap)
 import Data.StrMap as StrMap
+import Data.Tuple (Tuple(..))
 import Genetics.Browser.Events.Types (Event)
 
 
@@ -34,6 +38,8 @@ type SinkConfig a = { eventName :: String
                     }
 
 newtype TrackSink a = TrackSink (StrMap (Json -> a))
+-- TODO TrackSink i o = TrackSink (StrMap (i -> o))
+-- and we get a profunctor which might be nice; could do the same on the SinkConfig
 
 derive instance functorTrackSink :: Functor TrackSink
 derive instance newtypeTrackSink :: Newtype (TrackSink a) _
@@ -50,17 +56,21 @@ makeTrackSink sc = TrackSink $ StrMap.singleton sc.eventName sc.eventFun
 
 makeTrackSinks :: ∀ a.
                   Array (SinkConfig a)
-               -> Maybe (TrackSink a)
+               -> Either String (TrackSink a)
 makeTrackSinks scs = do
-  let sinks = foldMap makeTrackSink scs
-  -- basic check to make sure there are no overlapping configs,
-  -- since they'd be removed by StrMap.union
-  guard $ length scs == (StrMap.size $ unwrap sinks)
-  pure $ sinks
+  let count :: StrMap Int
+      count = StrMap.fromFoldableWith (+) $ map (\c -> Tuple c.eventName 1) scs
+      overlapping = StrMap.filter (_ > 1) count
+
+  when (not StrMap.isEmpty overlapping)
+    let error = foldMap (append "\n" <<< show) $ StrMap.keys overlapping
+    in throwError $ "Overlapping tracksinks!\n" <> error
+
+  pure $ foldMap makeTrackSink scs
 
 
-runSink :: ∀ a. TrackSink a -> Event -> Maybe a
-runSink (TrackSink sink) event = do
+runTrackSink :: ∀ a. TrackSink a -> Event -> Maybe a
+runTrackSink (TrackSink sink) event = do
   f <- StrMap.lookup event.name sink
   pure $ f event.evData
 
@@ -73,7 +83,7 @@ forkTrackSink :: ∀ env eff.
 forkTrackSink sink env bus = forkAff $ forever do
   event <- Bus.read bus
   let effect :: Maybe (env -> Eff (avar :: AVAR | eff) Unit)
-      effect = (map unsafeCoerceEff) <$> runSink sink event
+      effect = (map unsafeCoerceEff) <$> runTrackSink sink event
 
   case effect of
     Nothing -> pure unit
