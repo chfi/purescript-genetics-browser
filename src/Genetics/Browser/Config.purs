784270bc80438222f4ecaab9bc34d18a90e13272
Christian Fischer <christian@chfi.se>
2017-10-09 09:50:18 +0200

    Add tracksink config to browserconfig

--- src/Genetics/Browser/Config.purs
+++ src/Genetics/Browser/Config.purs
@@ -5,37 +5,47 @@ module Genetics.Browser.Config
 
 import Prelude
 
-import Control.Monad.Except (except, throwError, withExcept)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Except (except, withExcept)
 import Data.Argonaut (Json)
 import Data.Bifunctor (lmap)
-import Data.Either (isLeft, Either(..))
-import Data.Foldable (any)
+import Data.Either (Either)
 import Data.Foreign (F, Foreign, ForeignError(..), readArray, readString, unsafeReadTagged)
 import Data.Foreign.Index ((!))
-import Data.Maybe (Maybe(..), isNothing)
+import Data.Maybe (Maybe(Just))
 import Data.StrMap (StrMap)
 import Data.Traversable (traverse)
 import Genetics.Browser.Biodalliance (BrowserConstructor, RenderWrapper)
 import Genetics.Browser.Biodalliance.Config (RendererInfo, parseRenderers)
+import Genetics.Browser.Biodalliance.Types (Biodalliance)
 import Genetics.Browser.Config.Track (TracksMap, readTracksMap)
-import Genetics.Browser.Events.TrackSource (SourceConfig, makeTrackSource, validateSourceConfig)
+import Genetics.Browser.Cytoscape.Types (Cytoscape)
+import Genetics.Browser.Events.TrackSink (SinkConfig)
+import Genetics.Browser.Events.TrackSource (SourceConfig, validateSourceConfig)
 import Unsafe.Coerce (unsafeCoerce)
 
 
 -- TODO `wrapRenderer` and `browser` should both be in a BD-specific config,
 -- and be optional if there are no BD tracks.
 -- | The configuration type for the whole browser
-newtype BrowserConfig = BrowserConfig { wrapRenderer :: RenderWrapper
+newtype BrowserConfig eb ec = BrowserConfig { wrapRenderer :: RenderWrapper
                                       , bdRenderers :: StrMap RendererInfo
                                       , browser :: BrowserConstructor
                                       , tracks :: TracksMap
                                       , events :: Maybe { bdEventSources :: Array SourceConfig
                                                         , cyEventSources :: Array SourceConfig
+                                                        , bdEventSinks :: Array (SinkConfig
+                                                                          (Biodalliance -> Eff eb Unit))
+                                                        , cyEventSinks :: Array (SinkConfig
+                                                                          (Cytoscape -> Eff ec Unit))
                                                         }
                                       }
 
 readJson :: Foreign -> F Json
 readJson f = pure $ unsafeCoerce f
+
+readTaggedWithError :: forall a. String -> String -> Foreign -> F a
+readTaggedWithError s e f = withExcept (append (pure $ ForeignError e)) $ unsafeReadTagged s f
 
 parseSourceConfig :: Foreign -> F SourceConfig
 parseSourceConfig f = do
@@ -47,28 +57,40 @@ parseSourceConfig f = do
          , rawTemplate
          }
 
-readTaggedWithError :: forall a. String -> String -> Foreign -> F a
-readTaggedWithError s e f = withExcept (append (pure $ ForeignError e)) $ unsafeReadTagged s f
+parseSinkConfig :: forall a. Foreign -> F (SinkConfig a)
+parseSinkConfig f = do
+  eventName <- f ! "eventName" >>= readString
+  eventFun <- f ! "eventFun" >>=
+              readTaggedWithError "Function" ("Error on sink config for event: " <> eventName)
+  pure $ { eventName, eventFun }
+
 
 eitherToF :: Either String ~> F
 eitherToF = except <<< lmap (pure <<< ForeignError)
 
-parseBrowserConfig :: Foreign -> F BrowserConfig
+parseBrowserConfig :: forall eb ec. Foreign -> F (BrowserConfig eb ec)
 parseBrowserConfig f = do
   wrapRenderer <- f ! "wrapRenderer" >>= readTaggedWithError "Function" "Error on 'wrapRenderer':"
   browser <- f ! "browser" >>= readTaggedWithError "Function" "Error on 'browser':"
   tracks <- f ! "tracks" >>= readTracksMap
 
   events <- do
-    evs <- f ! "eventSources"
-    bd <- evs ! "bd" >>= readArray >>= traverse parseSourceConfig
-    cy <- evs ! "cy" >>= readArray >>= traverse parseSourceConfig
+    sources <- f ! "eventSources"
+    bdSources <- sources ! "bd" >>= readArray >>= traverse parseSourceConfig
+    cySources <- sources ! "cy" >>= readArray >>= traverse parseSourceConfig
 
-    _ <- eitherToF $ traverse validateSourceConfig bd
-    _ <- eitherToF $ traverse validateSourceConfig cy
+    _ <- eitherToF $ traverse validateSourceConfig bdSources
+    _ <- eitherToF $ traverse validateSourceConfig cySources
 
-    pure $ Just $ { bdEventSources: bd
-                  , cyEventSources: cy
+    sinks <- f ! "eventSinks"
+    bdSinks <- sinks ! "bd" >>= readArray >>= traverse parseSinkConfig
+    cySinks <- sinks ! "cy" >>= readArray >>= traverse parseSinkConfig
+    
+
+    pure $ Just $ { bdEventSources: bdSources
+                  , cyEventSources: cySources
+                  , bdEventSinks: bdSinks
+                  , cyEventSinks: cySinks
                   }
 
   bdRenderers <- f ! "renderers" >>= parseRenderers
