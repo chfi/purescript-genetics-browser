88d99c887696ac06529913f7dd029f7a56db2f39
Christian Fischer <christian@chfi.se>
2017-12-02 17:02:57 +0100

    Add UI.js...

---
+++ src/Genetics/Browser/Track/UI.js
@@ -0,0 +1,150 @@
+"use strict";
+
+exports.clearCanvas = function(canvas) {
+    return function() {
+        canvas.width = canvas.width;
+    };
+};
+
+exports.buttonEvent = function(id) {
+    return function(sub) {
+        var el = document.getElementById(id);
+        el.addEventListener('click', function(ev) {
+            sub();
+        });
+    };
+};
+
+exports.effEvent = function() {
+    var fun = null;
+    var ev = function(sub) {
+        fun = function() {
+            sub();
+        };
+    };
+    return { ev: ev, fun: fun };
+};
+
+exports.getScreenSize = function() {
+    var w = window.innerWidth;
+    var h = window.innerHeight;
+    return { w: w, h: h };
+};
+
+
+// scrolls a canvas, given a "back buffer" canvas to copy the current context to
+// TODO: later, do all heavy lifting on the backcanvas and just draw that to the screen
+exports.scrollCanvas = function(backCanvas) {
+    return function(canvas) {
+        return function(p) {
+            return function() {
+                // for some reason, doing this in newCanvas() below doesn't stick
+                backCanvas.width = canvas.width;
+                backCanvas.height = canvas.height;
+
+                var bCtx = backCanvas.getContext('2d');
+                var ctx = canvas.getContext('2d');
+
+                bCtx.drawImage(canvas, 0, 0);
+
+                // clears the canvas...
+                // no idea of performance vs. drawRect
+                canvas.width = canvas.width;
+                ctx.drawImage(backCanvas, p.x, p.y);
+            };
+
+        };
+    };
+};
+
+
+exports.newCanvas = function(size) {
+    return function() {
+        var c = document.createElement('canvas');
+        return c;
+    };
+};
+
+var evToPoint = function(e) {
+    return { x: e.clientX,
+             y: e.clientY
+           };
+}
+
+exports.canvasEvent = function(type) {
+    return function(canvas) {
+        return function(sub) {
+            var cb = function(e) {
+                sub(evToPoint(e));
+            };
+
+            canvas.addEventListener(type, cb);
+            return function() {
+                canvas.removeEventListener(type, cb);
+            }
+        };
+    };
+};
+
+
+exports.canvasDragImpl = function(canvas) {
+    return function(sub) {
+        var cb = function(e) {
+            var startX = e.clientX;
+            var startY = e.clientY;
+            var lastX = e.clientX;
+            var lastY = e.clientY;
+
+            var f = function(e2) {
+                sub({during: {x: lastX - e2.clientX, y: lastY - e2.clientY}});
+                lastX = e2.clientX;
+                lastY = e2.clientY;
+            };
+
+            document.addEventListener('mousemove', f);
+
+            document.addEventListener('mouseup', function(e2) {
+                document.removeEventListener('mousemove', f);
+                sub({total: {x: e2.clientX-startX, y: e2.clientY-startY}});
+            }, { once: true });
+        };
+
+        canvas.addEventListener('mousedown', cb);
+        return function() {
+            canvas.removeEventListener('mousedown', cb);
+        }
+    };
+};
+
+exports.setViewUI = function(contents) {
+    return function() {
+        var el = document.getElementById("view");
+        el.innerHTML = contents;
+    };
+};
+
+
+exports.canvasWheelEvent = function(canvas) {
+    return function(sub) {
+        var cb = function(e) {
+            sub(e.deltaY);
+        };
+
+        canvas.addEventListener("wheel", cb);
+        return function() {
+            canvas.removeEventListener("wheel", cb);
+        }
+    };
+};
+
+exports.subscribeM = function(dict) {
+    return function(e) {
+        return function(f) {
+            return function() {
+                return e(function(a) {
+                    dict.liftEff(f)(a)();
+                });
+            };
+        };
+    };
+};
