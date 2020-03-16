565a9ba24e738c66bf6ed337032604db52c30f29
Christian <christian@chfi.se>
2020-03-16 11:10:33 -0500

    Add tube map module, run from index.html

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const GenomeBrowser = require('./output/Genetics.Browser.UI');
 
 window.GenomeBrowser = GenomeBrowser;
+
+const tubemap = require('./js/tubemap.js');
+const examplevg = require('./example-vg.json');
+
+window.tubemap = tubemap;
+window.examplevg = examplevg;
